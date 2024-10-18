adb91ae2bf1012b04fbea57cb17d8ee6287d51eb
MBURU <wawerumburu157@gmail.com>
2024-10-18 20:06:54 +0300

    array-lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+} 
+function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+function appendCat(name) {
+    return [...cats, name]; 
+}
+function prependCat(name) {
+    return [name, ...cats ]; 
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
+function removeFirstCat() {
+    return cats.slice(1);
+    }
